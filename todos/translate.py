b3aa17fa9d6ce1740a756fbd491b5ce9c9289cf8
Quique <iacutetres@gmail.com>
2021-05-08 20:00:58 +0000

    translate feature

---
+++ todos/translate.py
@@ -0,0 +1,59 @@
+
+import os
+import json
+import logging
+
+from todos import decimalencoder
+import boto3
+dynamodb = boto3.resource('dynamodb')
+translateaws = boto3.client('translate')
+comprehend = boto3.client('comprehend')
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def comprehendText(data):
+    try:
+        response=comprehend.detect_dominant_language(Text = data)
+        return response['Languages'][0]['LanguageCode']
+    except Exception as e:
+        logger.error(response)
+        raise Exception("[ErrorMessageComprehend]: " + str(e))
+
+def translateText(data,source,target):
+    try:
+        response=translateaws.translate_text(Text = data,SourceLanguageCode=source, TargetLanguageCode=target)
+        return response['TranslatedText']  
+    except Exception as e:
+        logger.error(response)
+        raise Exception("[ErrorMessageTranslate]: " + str(e))
+        
+
+def translate(event, context):
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+    lang = event['pathParameters']['lang']
+    
+
+    # fetch todo from the database
+    result = table.get_item(
+        Key={
+            'id': event['pathParameters']['id']
+        }
+    )
+    
+    sourceLang = comprehendText(result['Item']['text'])
+    text = translateText(result['Item']['text'],sourceLang, lang)
+    r=result['Item']
+    # python object to be appended
+    l={'lang':lang,'detectedLang':sourceLang,'text':text}
+    # appending the data
+    r.update(l)
+ 
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(r,
+                           cls=decimalencoder.DecimalEncoder)
+    }
+
+    return response
